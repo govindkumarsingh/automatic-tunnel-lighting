11bf8fa8106a93d1feed2ecd728844ddad130f64
govindkumarsingh <41116654+govindkumarsingh@users.noreply.github.com>
2019-07-03 20:12:27 +0530

    Add files via upload

---
+++ goprajMega.ino
@@ -0,0 +1,53 @@
+#include <LiquidCrystal.h> // includes the LiquidCrystal Library
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+const int trigPin = 8;
+const int echoPin = 9;
+const int ledYellow=10;
+const int ledRed=11;
+
+long duration;
+int distance;
+void setup() {
+lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+pinMode(ledYellow, OUTPUT);
+pinMode(ledRed, OUTPUT);
+}
+void loop() {
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance= duration*0.034/2;
+Serial.println(distance);
+if(distance<13)
+{
+lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed
+lcd.print("     WELCOME   "); // Prints string "Distance" on the LCD
+lcd.setCursor(0,1);
+lcd.print("   IEEE-TUNNEL ");
+}
+else
+{
+  lcd.setCursor(0,0);
+  lcd.print("   TATHVA 2k17  ");
+  lcd.setCursor(0,1);
+  lcd.print("  SAVE WILDLIFE ");
+}
+
+if(distance<13)
+{
+  digitalWrite(ledYellow,HIGH);
+  digitalWrite(ledRed,LOW);
+  delay(500);
+}
+else
+{
+  digitalWrite(ledYellow,LOW);
+  digitalWrite(ledRed,HIGH);
+  delay(500);
+}
+}
---
+++ goprajuno.ino
@@ -0,0 +1,68 @@
+
+
+const int trigPin1 = 4;
+const int echoPin1 = 5;
+const int echoPin = 7;
+const int trigPin = 6;
+int relayPin1=13;
+int relayPin2=10;
+
+long duration;
+int distance,distance1;
+void setup() {
+pinMode(trigPin, OUTPUT); 
+pinMode(trigPin1, OUTPUT);
+pinMode(echoPin, INPUT); 
+pinMode(echoPin1, INPUT);
+Serial.begin(9600); 
+pinMode(relayPin1,OUTPUT);
+pinMode(relayPin2,OUTPUT);
+}
+void loop() {
+
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delay(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+
+Serial.print("Distance: ");
+Serial.println(distance);
+
+// Clears the trigPin
+digitalWrite(trigPin1, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin1, HIGH);
+delay(10);
+digitalWrite(trigPin1, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin1, HIGH);
+
+distance1= duration*0.034/2;
+
+Serial.print("Distance1: ");
+Serial.println(distance1);
+
+if(distance<10)
+{
+  digitalWrite(relayPin1,HIGH);
+  delay(500);
+}
+else
+{digitalWrite(relayPin1,LOW);
+}
+if(distance1<12)
+{
+  digitalWrite(relayPin2,HIGH);
+  delay(750);
+}
+else
+{digitalWrite(relayPin2,LOW);
+}
+}
